3d2fb15452054d023b4962090d506c24b2ba9638
Arpit <ak30062004@gmail.com>
2024-03-25 16:10:29 +0530

    Home Page Done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,29 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+        <img src="/code-sync.png" alt="code-sync-logo" />
+        <h4 className="mainLabel">Paste invitattion Room Id</h4>
+        <div className="inputGroup">
+          <input type="text" className="inputBox" placeholder="Room ID" />
+          <input type="text" className="inputBox" placeholder="UserName" />
+          <button className="btn joinBtn">Join</button>
+          <span className="createInfo">
+            If you don't have an invite then create &nbsp;
+            <a href="" className="createNewBtn">
+              New Room
+            </a>
+          </span>
+        </div>
+      </div>
+      <footer>
+        <h4>
+          Built with love by{" "}
+          <a href="https://github.com/Arpit-Kumar231">Arpit</a>
+        </h4>
+      </footer>
+    </div>
+  );
 }
