505773052ab3465f78b13a583bd9c99a3e57157e
Arpit <ak30062004@gmail.com>
2024-03-26 09:47:10 +0530

    Added codemirror

---
+++ src/components/Editor.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef } from "react";
+
+
+const Editor = () => {
+  const editorRef = useRef(null);
+  useEffect(() => {
+    async function init() {
+      editorRef.current = Codemirror.fromTextArea(
+        document.getElementById("realtimeEditor"),
+        {
+          mode: { name: "javascript", json: true },
+          theme: "dracula",
+          autoCloseTags: true,
+          autoCloseBrackets: true,
+          lineNumbers: true,
+        }
+      );
+
+      
+
+  return <textarea id="realtimeEditor"></textarea>;
+};
+
+export default Editor;
