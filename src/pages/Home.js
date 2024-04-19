8516749168606958ce2cd60a76567362fc1f8e40
Arpit <ak30062004@gmail.com>
2024-04-19 14:33:14 +0530

    Changes to home js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,60 +3,64 @@ import { v4 as uuidV4 } from "uuid";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
-export default function Home() {
-  const [roomId, setRoomId] = useState("");
-  const [username, setusername] = useState("");
+const Home = () => {
   const navigate = useNavigate();
 
+  const [roomId, setRoomId] = useState("");
+  const [username, setUsername] = useState("");
   const createNewRoom = (e) => {
     e.preventDefault();
     const id = uuidV4();
     setRoomId(id);
     toast.success("Created a new room");
   };
-  function join() {
+
+  const joinRoom = () => {
     if (!roomId || !username) {
-      toast.error("Room ID and Username is required");
+      toast.error("ROOM ID & username is required");
       return;
     }
+
+    // Redirect
     navigate(`/editor/${roomId}`, {
       state: {
         username,
       },
     });
-  }
-  function handleInputEnter(e) {
-    if (e.code === "Enter") {
-      join();
-    }
-  }
+  };
 
+  const handleInputEnter = (e) => {
+    if (e.code === "Enter") {
+      joinRoom();
+    }
+  };
   return (
     <div className="homePageWrapper">
       <div className="formWrapper">
-        <img src="/code-sync.png" alt="code-sync-logo" />
-        <h4 className="mainLabel">Paste invitation Room Id</h4>
+        <img
+          className="homePageLogo"
+          src="/code-sync.png"
+          alt="code-sync-logo"
+        />
+        <h4 className="mainLabel">Paste invitation ROOM ID</h4>
         <div className="inputGroup">
           <input
             type="text"
             className="inputBox"
-            placeholder="Room ID"
+            placeholder="ROOM ID"
+            onChange={(e) => setRoomId(e.target.value)}
             value={roomId}
-            onChange={(e) => {
-              setRoomId(e.target.value);
-            }}
+            onKeyUp={handleInputEnter}
           />
           <input
             type="text"
             className="inputBox"
-            placeholder="UserName"
+            placeholder="USERNAME"
+            onChange={(e) => setUsername(e.target.value)}
             value={username}
-            onChange={(e) => {
-              setusername(e.target.value);
-            }}
             onKeyUp={handleInputEnter}
           />
-          <button className="btn joinBtn" onClick={join}>
+          <button className="btn joinBtn" onClick={joinRoom}>
             Join
           </button>
           <span className="createInfo">
@@ -69,4 +73,6 @@ export default function Home() {
       </div>
     </div>
   );
-}
+};
+
+export default Home;
